974102af644eed585a46caee500bf1f785ad8bab
jueyang <jueyang22@gmail.com>
2014-04-04 12:37:32 -0400

    old maps must go

--- site.js
+++ site.js
@@ -4,20 +4,6 @@
 {% include js/jquery-1.8.0.min.js %}
 {% include js/lettering.js %}
 
-var loadMap = function(options) {
-    var markerLayer = mapbox.markers.layer().url('http://a.tiles.mapbox.com/v3/' + options.layer + '/markers.geojsonp');
-    mapbox.markers.interaction(markerLayer);
-
-    var heyMaps = mapbox.map(options.map, [mapbox.layer().id(options.layer), markerLayer], null, [
-            easey_handlers.TouchHandler(),
-            easey_handlers.DragHandler(),
-            easey_handlers.DoubleClickHandler()
-        ]);
-
-        heyMaps.zoom(options.zoom).center({ lat:options.lat, lon:options.lon });
-        heyMaps.setZoomRange(options.min,options.max);
-};
-
 var smallScreen = function() {
     return($(window).width()<=645)
 };
